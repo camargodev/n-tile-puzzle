b9f8487f2cb50b73b9d755238fafe8c61fba7e67
camargodev <camargodev@gmail.com>
2019-04-21 23:39:28 -0300

    Added results for report

--- 15-tile-test.sh
+++ 15-tile-test.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-while read p; do ./main -astar "$p"; done < input/15puzzle_instances.txt
+ulimit -t 30
+ulimit -v 8000000
+counter=0
+while read p; 
+do 
+    counter=$[$counter+1]
+    printf "\n$counter: "
+    ./main -astar "$p"; 
+done < input/15puzzle_instances.txt
 
